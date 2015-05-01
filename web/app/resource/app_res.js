e70aa33a1d3c89f6f06f40b9f2dac5beb94b4090
Phani Raj <phanirajuyn@gmail.com>
2015-05-01 11:01:00 -0700

    Make github login mandatory

--- web/app/resource/app_res.js
+++ web/app/resource/app_res.js
@@ -5,20 +5,33 @@ var BaseRes = require('./base_res'),
 
 var AppRes = module.exports = BaseRes.extend({
   route: function (app) {
+
+    app.get('/', _.bind(this.home, this));
     app.get('/stats/:username', _.bind(this.all, this));
+  },
+
+  home: function (req, res) {
+    res.render('app/home');
   },
 
   all: function (req, res) {
     var client = new GitClient(),
       user = {
+        gitUser: {},
+        properties: {
+          stats: []
+        },
         username: req.params.username
       },
       token = '';
 
     user.isAuthenticated = false;
     if (req.isAuthenticated()) {
+      console.log('user ', username, 'is authenticated');
       user.isAuthenticated = true;
       token = req.user.token;
+    } else {
+      return res.render('app/index', user);
     }
 
     async.waterfall([
